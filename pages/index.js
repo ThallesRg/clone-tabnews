eea3014590d3707e4c298b276d910a8643afe86f
thalles guimarães <thalles.guimaraes456@gmail.com>
2023-11-19 13:17:53 -0300

    masolhaso

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+    return <div>
+            <h1>Se você está lendo essa frase significa que a vercel é uma coisa linda, de outro universo, supimpa</h1>
+            <h2>Olha o H222</h2>
+            <h3>Olha o H333</h3>
+            <h4>Olha o H444</h4>
+            <h5>Olha o H555</h5>
+            <h6>O h6 é tão pequeno que nem da pra ler né, coisa doida</h6>
+        </div>
+}
+
+export default Home;
