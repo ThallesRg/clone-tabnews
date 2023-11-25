068fc21de7987f97b4be7769cd123ca6bceb4f0f
thalles guimarães <thalles.guimaraes456@gmail.com>
2023-11-25 00:37:41 -0300

    aaaaaaaaa

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>É preciso amaaaaaaaar as pessoas comosenaoouveseamanha</h1>
+    return <h1>É preciso amaaaaaaaar as pessoas comosenaoouveseamanha a vercel é doida</h1>
 }
 
 export default Home;
