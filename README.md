2f527c7fa159c67f1011d00f9970606f6db99b69
thalles guimarães <thalles.guimaraes456@gmail.com>
2023-11-27 00:30:09 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um repositório para testar novos conceitos de programação!!
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>É preciso amaaaaaaaar as pessoas comosenaoouveseamanha a vercel é doida</h1>
+  return (
+    <h1>
+      É preciso amaaaaaaaar as pessoas comosenaoouveseamanha a vercel é doida
+    </h1>
+  );
+}
+
+function BomDia() {
+  console.log("maoi");
 }
 
 export default Home;
